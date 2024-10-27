dba0cf957b3e34744f49035ab82bb4cd887712cd
BreicheSinal <sinalbreiche@gmail.com>
2024-10-27 16:51:08 +0200

    Completed pyramid function

---
+++ scripts/pyramid.js
@@ -0,0 +1,22 @@
+var enterBttn = document.getElementById('enterBttn');
+var input = document.getElementById('numberInput');
+var outputSec = document.getElementById('outputSection')
+
+enterBttn.addEventListener('click', pyramid);
+
+function pyramid(){
+    var inputValue = input.value;
+    var output = []; 
+
+    for (var x = 1; x <= inputValue; x++) {
+        var oddNum = 1;
+        for (var y = 0; y < x; y++) {
+            output.push(oddNum); 
+            output.push(' '); 
+            oddNum += 2;
+        }
+        output.push('\n'); 
+    }
+    console.log(output.join(''));
+    outputSec.textContent = output.join('');
+};
