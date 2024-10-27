d21181ca303148ed5d53a3d147daab6e969785d3
BreicheSinal <sinalbreiche@gmail.com>
2024-10-28 00:07:34 +0200

    Cleaned code | Added some styles (colors) | Added input validation

--- scripts/pyramid.js
+++ scripts/pyramid.js
@@ -1,22 +1,32 @@
 var enterBttn = document.getElementById('enterBttn');
 var input = document.getElementById('numberInput');
 var outputSec = document.getElementById('outputSection')
- 
+
 enterBttn.addEventListener('click', pyramid);
 
 function pyramid(){
     var inputValue = input.value;
     var output = []; 
 
-    for (var x = 1; x <= inputValue; x++) {
-        var oddNum = 1;
-        for (var y = 0; y < x; y++) {
-            output.push(oddNum); 
-            output.push(' '); 
-            oddNum += 2;
-        }
-        output.push('\n'); 
+    if(inputValue == 0 || inputValue <= 0)
+    {
+        outputSec.style.color = 'darkcyan';
+        outputSec.textContent = 'ENTER A NON ZERO NUMBER MATE ;)';
+        setTimeout(() =>{
+            location.reload();
+        }, 1777);
     }
-    console.log(output.join(''));
-    outputSec.textContent = output.join('');
+    else{
+        for (var x = 1; x <= inputValue; x++) {
+            var oddNum = 1;
+            for (var y = 0; y < x; y++) {
+                output.push(oddNum); 
+                output.push(' '); 
+                oddNum += 2;
+            }
+            output.push('\n'); 
+        }
+        console.log(output.join(''));
+        outputSec.textContent = output.join('');
+    }
 };
